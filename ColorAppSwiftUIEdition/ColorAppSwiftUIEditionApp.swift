6aafb2110b6e5f695751050bbc6670c4a3166c70
AkiraRei <Akirarei@yandex.ru>
2023-10-09 16:47:36 +0300

    Initial Commit

---
+++ ColorAppSwiftUIEdition/ColorAppSwiftUIEditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppSwiftUIEditionApp.swift
+//  ColorAppSwiftUIEdition
+//
+//  Created by Акира on 09.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppSwiftUIEditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
